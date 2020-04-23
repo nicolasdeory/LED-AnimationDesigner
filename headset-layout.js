718438b5851ce87161402479239af4fdee1d3c9c
Nicolás de Ory <deorynicolas@gmail.com>
2020-04-23 23:30:14 +0200

    Added Headset Layout

---
+++ headset-layout.js
@@ -0,0 +1,16 @@
+HEADSET_LAYOUT = [
+    {
+        "key":"",
+        "x":0,
+        "y":0,
+        "width":8,
+        "height":40
+    },
+    {
+        "key":"",
+        "x":40,
+        "y":0,
+        "width":8,
+        "height":40
+    },
+]
